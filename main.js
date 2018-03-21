ade31c2af9b306845be8af926aa45f8ac175cbf6
Julieneri <julien.erisay@essec.edu>
2018-03-21 17:49:56 +0100

    package port 1999

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongo = require('mongodb')
+const app = express();
+
+app.use(express.static('static'))
+
+app.get('/help', (req, res) => res.send('Coding is hard!'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(1999, () => console.log('Example app listening on port 1999!'));
